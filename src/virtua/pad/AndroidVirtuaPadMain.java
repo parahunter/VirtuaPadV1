a945090076c4f236a2530db783c290aff280ac69
Maya Saietz <mayasaietz@gmail.com>
2012-01-16 22:44:07 +0100

    First commit

---
+++ src/virtua/pad/AndroidVirtuaPadMain.java
@@ -0,0 +1,114 @@
+package virtua.pad;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.hardware.*;
+
+import java.io.IOException;
+import java.net.*;
+
+public class AndroidVirtuaPadMain extends Activity implements SensorEventListener
+{
+	enum clientState{disconnected, tryingToConnect, connected};
+	
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+	private TextView tw;
+    private DatagramSocket udpSocket;
+    private Socket tcpSocket;
+    int serverPort;
+    InetAddress serverAddress;
+    private clientState state;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+    	    	
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        
+        tw = new TextView(this);
+        state = clientState.disconnected;
+    	serverPort = 9999;
+    	serverAddress = null;
+    	
+		try
+    	{
+			serverAddress = InetAddress.getByName("192.168.1.100");
+    		
+        	udpSocket = new DatagramSocket();
+        	
+        	tcpSocket = new Socket(serverAddress, serverPort);
+        	
+    	}
+    	catch(SocketException e)
+    	{
+    		Log.e("socket", e.getMessage(), e);
+    	}
+    	catch(UnknownHostException e)
+    	{
+    		Log.e("host", "blah2", e);
+    	} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                
+        setContentView(tw);
+        
+    }
+    
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+    
+	public void onAccuracyChanged(Sensor sensor, int accuracy) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void onSensorChanged(SensorEvent event) 
+	{
+		if(state != clientState.connected)
+			return;
+		
+    	//int packetLength = 96;
+    	//byte[] data = new byte[packetLength];
+    	
+    	tw.setText(event.values[0] + " " + event.values[1] + " " + event.values[2]);
+    	/*        
+        byte[] data = 
+        		
+        DatagramPacket p = new DatagramPacket(message, msg_length,address,port);
+        
+        try 
+        {
+        	//Log.v("UDP send", address.toString() + " " + port);
+        	socket.send(p);
+		} 
+        catch (IOException e) 
+        {
+			// TODO Auto-generated catch block
+			Log.e("UDP", e.getMessage(), e);
+        	e.printStackTrace();
+		}*/
+	}
+	
+	
+}
